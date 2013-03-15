d0b07f01cc381957dc44c337234ec446fb6e904f
softprops <d.tangren@gmail.com>
2013-03-14 21:36:00 -0400

    thoughts on writer

--- src/main/scala/file.scala
+++ src/main/scala/file.scala
@@ -15,4 +15,16 @@ object File {
     }
     private def local(path: String) = None
   }
+
+  // ideally, this should be `pipeable`
+  // meaning we should be able to create
+  // and instance of this and pipe it
+  // a stream of har entries
+  // rather than creating a whole tree in memory
+  // then serializing it in bulk.
+  // I'm not sure how that would be possible 
+  // using json4s. think about it.
+  object Writer {
+    // def appender(...)
+  }
 }
